f33e838d49d9b6c3f1ef600dbd89b24d30413f6c
Zaidis <chewybinx@gmail.com>
2022-01-24 19:35:25 -0600

    Finished Door Script
    - Door Script complete
    - Basic door 1 prefab in prefab folder

--- GGJ_Duality/Assets/Script/Triggers/Door.cs
+++ GGJ_Duality/Assets/Script/Triggers/Door.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Door : Triggerable
 {
+    [Header("Door Variables")]
+    //door objects
+    [SerializeField] private GameObject leftDoor;
+    [SerializeField] private GameObject rightDoor;
 
     [SerializeField] private Transform leftClosedState;
     [SerializeField] private Transform leftOpenState;
@@ -11,35 +15,65 @@ public class Door : Triggerable
     [SerializeField] private Transform rightClosedState;
     [SerializeField] private Transform rightOpenState;
 
-    [SerializeField] private float speed;
-
+    [SerializeField] private float totalTime;
+    [SerializeField] private bool opened;
     
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
-            if (CheckIfDoorCanOpen()) {
-
+            Debug.Log("Player is near the door!");
+            if (!opened) { //if not opened
+                if (CheckIfActive()) {
+                    //checks to see if the door can open
+                    StopAllCoroutines();
+                    StartCoroutine(OpenDoor(totalTime));
+                }
             }
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
-
+            if (opened) {
+                StopAllCoroutines();
+                StartCoroutine(CloseDoor(totalTime));
+            }
         }
     }
 
-    /// <summary>
-    /// Checks to see if the door is unlocked. 
-    /// </summary>
-    public bool CheckIfDoorCanOpen() {
+    IEnumerator OpenDoor(float time) {
+        opened = true;
+        Vector3 startingLeftPos = leftDoor.transform.position;
+        Vector3 startingRightPos = rightDoor.transform.position;
 
+        float elapsedTime = 0;
+
+        while (elapsedTime < time) {
+            //leftSide
+            leftDoor.transform.position = Vector3.Lerp(startingLeftPos, leftOpenState.position, (elapsedTime / time));
+            rightDoor.transform.position = Vector3.Lerp(startingRightPos, rightOpenState.position, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        leftDoor.transform.localPosition = leftOpenState.localPosition;
+        rightDoor.transform.localPosition = rightOpenState.localPosition;
     }
 
-    IEnumerator OpenDoor() {
+    IEnumerator CloseDoor(float time) {
+        opened = false;
+        Vector3 startingLeftPos = leftDoor.transform.position;
+        Vector3 startingRightPos = rightDoor.transform.position;
 
-    }
+        float elapsedTime = 0;
 
-    IEnumerator CloseDoor() {
-
-    }
+        while (elapsedTime < time) {
+            //leftSide
+            leftDoor.transform.position = Vector3.Lerp(startingLeftPos, leftClosedState.position, (elapsedTime / time));
+            rightDoor.transform.position = Vector3.Lerp(startingRightPos, rightClosedState.position, (elapsedTime / time));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        leftDoor.transform.localPosition = leftClosedState.localPosition;
+        rightDoor.transform.localPosition = rightClosedState.localPosition;
+    } 
 }
--- GGJ_Duality/Assets/Script/Triggers/Triggerable.cs
+++ GGJ_Duality/Assets/Script/Triggers/Triggerable.cs
@@ -3,13 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Triggerable : MonoBehaviour {
+    [Header("Triggerable Variables")]
     [SerializeField] List<Trigger> triggers;
     public bool active;
-    private void Update() {
+   /* private void Update() {
         int count = 0;
-        for (int i = 0; i < triggers.Count - 1; i++)
-            if (triggers[i] != null)
-                if (triggers[i].active) count++;
+        for (int i = 0; i < triggers.Count - 1; i++) {
+            if (triggers[i] != null) {
+                if (triggers[i].active) {
+                    count++;
+                }
+            }
+        }
         active = (count == triggers.Count);
+    } */
+
+    public bool CheckIfActive() {
+        int count = 0;
+        for (int i = 0; i < triggers.Count - 1; i++) {
+            if (triggers[i] != null) {
+                if (triggers[i].active) {
+                    count++;
+                }
+            }
+        }
+        active = (count == triggers.Count);
+        return active;
     }
 }
